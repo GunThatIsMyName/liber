343b9028e19af46bd4e6d5d749edcae50ba8f042
Minji <321963741@naver.com>
2021-11-26 08:07:43 +0900

    done!

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,7 +18,7 @@ const FeaturedProducts = () => {
             .slice(0, 3)}
         </div>
       </div>
-      <Link to="/shop/2020" className="btn">
+      <Link to="/shop/2021" className="btn">
         All Collections
       </Link>
     </Wrapper>
