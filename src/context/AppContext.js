0bb958d045dfa14ee914ebc3117a9253ff76181e
이준현 <cono@ijunhyeon-ui-Macmini.local>
2024-12-01 21:47:03 +0900

    feat: 2024 collections updated

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,9 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useReducer,
-  useState,
-} from 'react';
+import React, { createContext, useContext, useReducer, useState } from 'react';
 import {
   clickCloseModal,
   clickOpenModal,
@@ -17,41 +12,69 @@ import {
   SIDEBAR_CLOSE,
   SIDEBAR_OPEN,
 } from './action';
-import reducer, {initialState} from './reducer';
+import reducer, { initialState } from './reducer';
 import axios from 'axios';
 
-const API_START_POINT = '/.netlify/functions';
+const API_START_POINT = 'https://api.airtable.com/v0/app1szHXcA5oPa7Pk';
 
 const AppContext = createContext();
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [showPhoto, setShowPhoto] = useState({});
 
   const fetchData = async (year) => {
-    dispatch({type: SET_LOADING});
+    dispatch({ type: SET_LOADING });
     try {
-      const {data} = await axios(`${API_START_POINT}/${year}`);
-      const yearData = data.sort((a, b) => a.month - b.month);
-      dispatch({type: GET_DATA, payload: yearData});
+      const { data } = await axios(`${API_START_POINT}/${year}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_TOKEN}`,
+        },
+      });
+
+      console.log(data, 'data');
+      const { records } = data;
+
+      if (!records || records.length === 0) {
+        throw new Error('No data');
+      }
+
+      const newData = records
+        .map((item) => {
+          const newImgData = item.fields.img.map((imgItem) => {
+            return {
+              id: imgItem.id,
+              url: imgItem.url,
+              name: imgItem.filename,
+            };
+          });
+          return {
+            id: item.id,
+            month: item.fields.month,
+            img: newImgData,
+          };
+        })
+        .sort((a, b) => a.month - b.month);
+
+      dispatch({ type: GET_DATA, payload: newData });
     } catch (error) {
-      dispatch({type:ERROR});
+      dispatch({ type: ERROR });
     }
   };
 
   const controlGridView = () => {
-    dispatch({type: SET_GRIDVIEW});
+    dispatch({ type: SET_GRIDVIEW });
   };
   const controlListView = () => {
-    dispatch({type: SET_LISTVIEW});
+    dispatch({ type: SET_LISTVIEW });
   };
 
   const openSidebar = () => {
-    dispatch({type: SIDEBAR_OPEN});
+    dispatch({ type: SIDEBAR_OPEN });
   };
 
   const closeSidebar = () => {
-    dispatch({type: SIDEBAR_CLOSE});
+    dispatch({ type: SIDEBAR_CLOSE });
   };
 
   const handleScrollTop = () => {
@@ -62,16 +85,16 @@ const AppProvider = ({children}) => {
   };
 
   const openModal = (id, idx, month) => {
-    setShowPhoto({id, idx, month});
-    dispatch({type: clickOpenModal, payload: {id, month}});
+    setShowPhoto({ id, idx, month });
+    dispatch({ type: clickOpenModal, payload: { id, month } });
   };
 
   const closeModal = () => {
-    dispatch({type: clickCloseModal});
+    dispatch({ type: clickCloseModal });
   };
 
   const handleClick = (event) => {
-    const {nodeName} = event.target;
+    const { nodeName } = event.target;
     if (nodeName === 'IMG') {
       return;
     }
@@ -83,14 +106,12 @@ const AppProvider = ({children}) => {
     console.log(volume, 'volume');
     if (volume) {
       video.muted = false;
-      dispatch({type: OFF_MUTED});
+      dispatch({ type: OFF_MUTED });
     } else {
       video.muted = true;
-      dispatch({type: ON_MUTED});
+      dispatch({ type: ON_MUTED });
     }
   };
-
-
 
   return (
     <AppContext.Provider
